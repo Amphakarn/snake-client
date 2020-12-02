92abaf1b4072647c4d38a6fd9545344366842af0
Amphakarn <pisuthigomol@yahoo.com>
2020-12-02 20:35:30 +0000

    added code to test the move command

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 const net = require('net');
 
-const connect = function() {
+const connect = function () {
   // Creates a TCP connection to port on host
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
     console.log('Successfully connected to game server: ', data);
@@ -17,7 +17,15 @@ const connect = function() {
     conn.write('Name: Bee');
   });
 
+  // conn.on('connect', () => {
+  //   for (let i = 0; i < 10; i++) {
+  //     setTimeout(() => {
+  //       conn.write('Move: up');
+  //     }, i * 100);
+  //   }  
+  // });
+
   return conn;
 }
 
-module.exports = {connect};
+module.exports = { connect };
