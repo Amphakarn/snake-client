6af569e5f229e27efedcb954fb4e1af547022d89
Amphakarn <pisuthigomol@yahoo.com>
2020-12-02 19:29:44 +0000

    added comment to the client.js

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net = require('net');
 
 const connect = function() {
+  // Creates a TCP connection to port on host
   const conn = net.createConnection({ 
     host: '135.23.222.131',
     port: 50542
@@ -9,7 +10,7 @@ const connect = function() {
   conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
-    console.log('Server says: ', data);
+    console.log('Successfully connected to game server: ', data);
   });
 
   conn.on('connect', () => {
